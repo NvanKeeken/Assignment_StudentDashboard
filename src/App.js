5fe8aea266a3c98d21c7bbd9516d378c46a0f609
N van Keeken <n.v.k20@gmail.com>
2022-12-18 20:31:52 +0100

    added bar chard + calculated average

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import "./App.css";
+import { useState } from "react";
+import { data } from "./data/data";
 
-import './App.css';
+import BarChard from "./chard/BarChard";
 
 function App() {
+
+   const [assignment, setAssignment] = useState([])
+
+ 
+
+  
+
+  const calculateAverageEnjoymentRate = () => {
+    const assignments = data.map((item) => item.assignment); // makes array from all assignments
+    const singleAssignments =Array.from(new Set(assignments)) // makes array with unique assignments
+
+    // for every assignment it will return an object with assignment, average difficultyRate and enjoymentrate
+     return singleAssignments.map((singleAssignment) => {
+      const seperateAssignment = data.filter((obj) => obj.assignment === singleAssignment);
+      
+      const averageEnjoymentRate =
+        seperateAssignment.reduce(
+          (prev, current) => prev + parseInt(current.enjoymentRate),
+          0
+        ) / seperateAssignment.length;
+      const averageDifficultyRate =
+        seperateAssignment.reduce(
+          (prev, current) => prev + parseInt(current.difficultyRate),
+          0
+        ) / seperateAssignment.length;
+      return {
+        assignment: `${seperateAssignment[0].assignment}`,
+        difficultyRate: averageDifficultyRate,
+        enjoymentRate: averageEnjoymentRate,
+      };
+    })
+   
+  };
+
+console.log(calculateAverageEnjoymentRate())
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <BarChard data={calculateAverageEnjoymentRate()}/>
     </div>
   );
 }
---
+++ src/chard/BarChard.js
@@ -0,0 +1,37 @@
+import React from "react"
+import {VictoryBar, VictoryChart, VictoryAxis,VictoryGroup} from "victory"
+
+function BarChard (props){
+
+    return(
+        <VictoryChart
+      domainPadding={50}
+      height={250}
+      width={900}
+      >
+        <VictoryAxis
+        style={{ ticks: {stroke: "grey", size: 5},
+        tickLabels: {angle: 45, fontSize: 5, padding: 5, textAnchor: 'begin'} }}
+        />
+      <VictoryGroup
+      offset={5}
+      colorScale={"qualitative"}
+      >
+      <VictoryBar
+      style={{ data: { fill: "#ffb212" } }}
+      data={props.data}
+      x={"assignment"}
+      y={"difficultyRate"}
+      />
+      <VictoryBar
+      data={props.data}
+      style={{ data: { fill: "#4f8bc9" } }}
+      x={"assignment"}
+      y={"enjoymentRate"}
+      />
+      </VictoryGroup>
+      </VictoryChart>
+    )
+}
+
+export default BarChard
